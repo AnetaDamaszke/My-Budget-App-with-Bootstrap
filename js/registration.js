387f23c279bbea9341bc1fe7f2e6e5860c79c6e9
Aneta Damaszke <aneta.damaszke.programista@gmail.com>
2022-02-23 20:03:43 +0100

    Script for registartion form added

---
+++ js/registration.js
@@ -0,0 +1,61 @@
+function checkName() {
+
+   var name = document.getElementById("name").value;
+   
+   if (name == "") {
+    document.getElementById("statement").innerHTML = "Podaj imię!"
+    return false;
+  }
+   else return true;
+}
+
+function validateEmail() {
+
+  var email = document.getElementById("email").value;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if(email.match(mailformat)) return true;
+  else {
+    document.getElementById("statement").innerHTML = "Niepoprawny adres e-mail!";
+    return false;
+  }
+}
+
+
+function checkPassword() {
+
+  var password1 = document.getElementById("password1").value;
+  var password2 = document.getElementById("password2").value;
+
+  if (password1 == "") {
+    document.getElementById("statement").innerHTML = "Nie podano hasła!" ;
+    return false;
+  }
+  else {
+    if (password2 == "") {
+      document.getElementById("statement").innerHTML = "Powtórz hasło!" ;
+      return false;
+    }
+    else {
+      if (password1 == password2) return true;
+    else {
+      document.getElementById("statement").innerHTML = "Podano różne hasła!" ;
+      return false;
+    }
+    } 
+  }
+}
+
+function validateForm() {
+  
+  var link = document.getElementById("registration");
+
+  if ((validateEmail() == true) && (checkPassword() == true) && (checkName() == true)) {
+    document.getElementById("statement").innerHTML = "Konto zostało pomyślnie utworzone!";
+    document.getElementById("registration").innerHTML = "Zaloguj";
+
+    //link.setAttribute("href", "login.html");
+
+  }
+
+}
